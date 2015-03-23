2644bbc371dc9e5850d0dbb2cb57c8f9b2e84f0f
Jazz-S <jazz1santiago@gmail.com>
2015-03-23 14:01:53 -0700

    Initial Commit

---
+++ MeetMeUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetMeUp
+//
+//  Created by Jazz Santiago on 3/23/15.
+//  Copyright (c) 2015 Orginization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetMeUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetMeUp
+//
+//  Created by Jazz Santiago on 3/23/15.
+//  Copyright (c) 2015 Orginization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
